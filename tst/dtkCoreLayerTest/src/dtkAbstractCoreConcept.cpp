52b5701b640ea47dbd5685b8544d370f07271b96
Thibaud Kloczko <thibaud.kloczko@inria.fr>
2018-07-24 09:02:05 +0200

    Add full fake layer to test all dtk-core features.

---
+++ tst/dtkCoreLayerTest/src/dtkAbstractCoreConcept.cpp
@@ -0,0 +1,29 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "dtkAbstractCoreConcept.h"
+
+#include "dtkCoreLayerTest.h"
+
+// ///////////////////////////////////////////////////////////////////
+// Register to the layer
+// ///////////////////////////////////////////////////////////////////
+
+namespace dtkCoreLayerTest
+{
+    DTK_DEFINE_CONCEPT(dtkAbstractCoreConcept, coreconcept, dtkCoreLayerTest);
+}
+
+//
+// dtkAbstractCoreConcept.cpp ends here
---
+++ tst/dtkCoreLayerTest/src/dtkCoreLayerTest.cpp
@@ -0,0 +1,64 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "dtkCoreLayerTest.h"
+
+#include <dtkLog>
+
+#include "dtkCoreLayerTestConfig.h"
+
+#include "dtkCoreLayerTestSettings.h"
+
+// /////////////////////////////////////////////////////////////////
+// Layer methods implementations
+// /////////////////////////////////////////////////////////////////
+
+namespace dtkCoreLayerTest {
+
+    DTK_DEFINE_LAYER_MANAGER;
+
+    void activateObjectManager(void) {
+        manager().setObjectManager(dtkCoreObjectManager::instance());
+    }
+
+    void initialize(const QString& s) {
+
+        QString path = s;
+        if (path.isEmpty()) {
+            dtkCoreLayerTestSettings settings;
+            settings.beginGroup("imaging");
+            path = settings.value("plugins").toString();
+            settings.endGroup();
+            if (path.isEmpty()) {
+                path = QDir::cleanPath(DTKCORELAYERTEST_INSTALL_PREFIX + "/plugins/dtkCoreLayerTest");
+                dtkDebug() << "no plugin path configured, use default:" << path;
+            }
+        }
+
+        manager().initialize(path);
+    }
+
+    void setVerboseLoading(bool b)
+    {
+        manager().setVerboseLoading(b);
+    }
+
+    void uninitialize(void)
+    {
+        manager().uninitialize();
+    }
+}
+
+//
+// dtkCoreLayerTest.cpp ends here
