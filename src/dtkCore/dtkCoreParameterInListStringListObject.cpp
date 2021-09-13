eb5a7662c5aad7dc82942891fbabee5105dee9ad
Thibaud Kloczko <thibaud.kloczko@inria.fr>
2021-09-13 15:53:45 +0200

    Add InlistStringList object.
    Need to complete the binding with the param.

---
+++ src/dtkCore/dtkCoreParameterInListStringListObject.cpp
@@ -0,0 +1,62 @@
+// dtkCoreParameterInListStringListObject.cpp
+//
+
+#include "dtkCoreParameterInListStringListObject.h"
+
+// ///////////////////////////////////////////////////////////////////
+// dtkCoreParameterInListStringListObject implementation
+// ///////////////////////////////////////////////////////////////////
+
+
+dtkCoreParameterInListStringListObject::dtkCoreParameterInListStringListObject(dtkCoreParameterInListStringList *p) : dtkCoreParameterObject(p), m_param(p)
+{
+
+}
+
+dtkCoreParameterInListStringListObject::~dtkCoreParameterInListStringList(void)
+{
+    m_param = nullptr;
+}
+
+QStringList dtkCoreParameterInListStringListObject::value(void) const
+{
+    return m_param->value();
+}
+
+void dtkCoreParameterInListStringListObject::setValue(const QStringList& v)
+{
+    m_param->setValue(v);
+}
+
+QStringList dtkCoreParameterInListStringListObject::list(void) const
+{
+    m_param->values();
+}
+
+void dtkCoreParameterInListStringListObject::setList(const QStringList& l)
+{
+    m_param->setValues(l);
+}
+
+int dtkCoreParameterInListStringListObject::size(void) const
+{
+    return m_param->size();
+}
+
+void dtkCoreParameterInListStringListObject::notifyValue(const QStringList& v)
+{
+    emit valueChanged(v);
+}
+
+void dtkCoreParameterInListStringListObject::notifyList(const QStringList& l)
+{
+    emit listChanged(l);
+}
+
+dtkCoreParameterInListStringList *dtkCoreParameterInListStringListObject::parameter(void)
+{
+    return m_param;
+}
+
+//
+// dtkCoreParameterInListStringListObject.h ends here
---
+++ src/dtkCore/dtkCoreParameterInListStringListObject.h
@@ -0,0 +1,51 @@
+// dtkCoreParameterInListStringListObject.h
+//
+
+#pragma once
+
+#include <dtkCoreExport>
+
+#include "dtkCoreParameterObject.h"
+
+#include "dtkCoreParameterInListStringList.h"
+
+// ///////////////////////////////////////////////////////////////////
+// dtkCoreParameterInListStringListObject declaration
+// ///////////////////////////////////////////////////////////////////
+
+class DTKCORE_EXPORT dtkCoreParameterInListStringListObject : public dtkCoreParameterObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QStringList value READ value WRITE setValue NOTIFY valueChanged)
+    Q_PROPERTY(QStringList list  READ list  WRITE setList  NOTIFY  listChanged)
+    Q_PROPERTY(int size READ size)
+
+public:
+    dtkCoreParameterInListStringListObject(dtkCoreParameterInListStringList *);
+    ~dtkCoreParameterInListStringList(void);
+
+    QStringList value(void) const;
+    void setValue(const QStringList&);
+
+    QStringList list(void) const;
+    void setList(const QStringList&);
+
+    int size(void) const;
+
+signals:
+    void valueChanged(const QStringList&);
+    void  listChanged(const QStringList&);
+
+public:
+    void notifyValue(const QStringList&);
+    void notifyList(const QStringList&);
+
+public:
+    dtkCoreParameterInListStringList *parameter(void) override;
+
+private:
+    dtkCoreParameterInListStringList *m_param = nullptr;
+};
+
+//
+// dtkCoreParameterInListStringListObject.h ends here
